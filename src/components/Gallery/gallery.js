97378bbc93776b6696e743e70370830565079878
tejasri-amaravathi <amaravathitejasri@gmail.com>
2024-07-01 19:54:38 +0530

    Add Image

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -1,12 +1,29 @@
-// src/components/Gallery.js
-
-import React from 'react';
+import React from "react";
+import "./gallery.css";
+import image1 from "../../images/galleryImage1.jpeg";
+import image2 from "../../images/galleryImage2.jpeg";
+import image3 from "../../images/galleryImage3.jpeg";
+import image4 from "../../images/galleryImage4.jpeg";
+import image5 from "../../images/galleryImage5.jpeg";
+import image6 from "../../images/galleryImage6.jpeg";
+import image7 from "../../images/galleryImage7.jpeg";
+import image8 from "../../images/galleryImage8.jpeg";
+import image9 from "../../images/galleryImage9.jpeg";
+import image10 from "../../images/galleryImage10.jpeg";
 
 const Gallery = () => {
   return (
     <div>
-      <h2>Gallery</h2>
-      <p>Our photo gallery will be displayed here.</p>
+      <img src={image1} className={"image"} />
+      <img src={image2} className={"image"} />
+      <img src={image3} className={"image"} />
+      <img src={image4} className={"image"} />
+      <img src={image5} className={"image"} />
+      <img src={image6} className={"image"} />
+      <img src={image7} className={"image"} />
+      <img src={image8} className={"image"} />
+      <img src={image9} className={"image"} />
+      <img src={image10} className={"image"} />
     </div>
   );
 };
--- src/components/Services/services.js
+++ src/components/Services/services.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React from "react";
 
 const Services = () => {
   return (
     <div>
-      <h2>Services</h2>
-      <p>Learn more about services</p>
+      <h2>History</h2>
+      <p>
+        Origins of Handloom Weaving: The origins of handloom weaving can be
+        traced back to the Neolithic era, where early humans began experimenting
+        with interlacing plant fibers to create basic fabrics. As civilization
+        progressed, handloom weaving techniques developed independently in
+        different regions of the world: 1. Founder of Handlooms: The origins of
+        handloom in the Indian subcontinent may be traced back to the Indus
+        Valley Civilization, according to archaeological findings. Weavers
+        received royal support with the rise of the Mughal empire, resulting in
+        the invention of new textiles such as 'Mulmul,' 'Benarsi Brocade,' and
+        'Jamawar,' among others.. 2. First Handloom Cloth in India: Pochampally
+        saree The Pochampally saree is the first handloom cloth patented in
+        India. 3. India's Famous Handloom: Varanasi is celebrated for its
+        exquisite handloom products, particularly the Benarasi silk sarees, zari
+        embroidery, and brocades. It is a hub for weaving crafts, where skilled
+        artisans weave gold and silver threads into fabrics commonly chosen for
+        Indian weddings The handloom industry produces a wide range of unique
+        designs and patterns. The handloom fabrics are known for their intricate
+        designs, vibrant colors, and high quality. The products are often valued
+        for their aesthetic appeal and cultural significance.
+      </p>
+
+      <h2>Awareness of handlooms</h2>
+      <p>
+        <b>Plan and design:</b> Handloom allows the weaver to have total control
+        of the creative process which encourages them to explore new ideas and
+        come up with innovative weaving patterns and designs. On the other hand,
+        power looms are repetitive and only allow the weaver to create a replica
+        of one particular design. Crafting A Greener.
+        <br />
+        <br />
+        <b>Canvas:</b> A Vision For Sustainable Handloom Trends In 2024 And
+        Beyond. In the intricate world of handloom textiles, 2024 beckons as a
+        pivotal year where sustainability takes center stage, steering the
+        industry towards a conscious and eco-friendly future.
+      </p>
+      <h2>Challenges to Handlooms:</h2>
+      <p>
+        They could not get sufficient supply of raw cotton which was sent to
+        England for their industries after American world war The prices of raw
+        cotton shot up and Indian weavers were forced to buy raw cotton at
+        exorbitant prices. In most cases, they were unable to pay. Other factors
+        such as rapid and manifold increase in the prices of raw material, poor
+        awareness of buying patterns/trends in the market, inadequate variety in
+        designs, low grade technology, and high cost of pro· duction have led to
+        handlooms becoming an unviable proposition. Climate change is a major
+        threat to the planet and its life forms, as it causes extreme weather
+        events, sea level rise, melting of ice caps, and shifts in ecosystems.
+        To limit global warming to 2°C or less, we need to reduce greenhouse gas
+        emissions and increase the use of renewable energy sources. The study
+        found that the main problems faced are lack of working capital,
+        increasing price of raw materials, stiff competition from the power loom
+        sector, lack of organizational capacity, and lack of policy support and
+        it also indicates that the handloom weavers are not happy with the
+        financial benefits of this ....
+      </p>
     </div>
   );
 };
