a7cd5657eb1d8dae9ba2d82163f2ff83b9cfc465
tejasri-amaravathi <amaravathitejasri@gmail.com>
2024-07-01 20:36:34 +0530

    Add advantages

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      <p>Welcome to the Home tab!</p>
+      <h2>Importance of Handlooms</h2>
+      <p>
+        Handloom sarees are a traditional textile art of Bangladesh and India.
+        The production of handloom saris is important for economic development
+        in rural India. Completion of a single sari takes two to three days of
+        work. Several regions have their own traditional styles of weaving
+        handloom sarees. 1. Handloom. A 'handloom' is a loom that is used to
+        weave cloth without the use of any electricity. Hand weaving is done on
+        pit looms or frame looms generally located in weavers' homes. Weaving is
+        primarily the interlacing of two sets of yarn – the warp (length) and
+        the weft (width). 2. Handloom sector is a symbol of the country's
+        glorious cultural heritage and an important source of livelihood in the
+        country. The sector is key to women empowerment as over 70% of handloom
+        weavers and allied workers are women.
+      </p>
+      <h2>Way to protect the Handloooms</h2>
+      <p>
+        For protection and promotion of handlooms, the Government has enacted
+        the Handlooms (Reservation of Articles for Production) Act, 1985 wherein
+        11 textiles articles with certain technical specifications are reserved
+        for exclusive production by handlooms 1.Eco-friendly: The handloom
+        industry is a perfect example of sustainable production. Every step of
+        the handloom process is sustainable, from installation, fabric choice,
+        dyeing and weaving. The natural fibers used are biodegradable and are
+        not produced with many chemicals and resources. 2.Sustainable: Hand
+        looms use less energy and resources than industrial looms, making them a
+        more eco-friendly option
+      </p>
+      <h2>Advantages of handloom fabrics</h2>
+      <p>
+        <b>uniquness:</b> Each item is work of art and a unique display of
+        manship
+        <br />
+        <b>variety:</b> Hand-looming is allows for creative weaving pattern and
+        designs
+        <br />
+        <b>Quality:</b> Handloom fabrics are made with high quality natural
+        fribes such as slik add cotton
+        <br />
+        <b>comfort:</b> Hanloom farbics are the skin more absorbent and
+        breathable
+        <br />
+        <b>skill preservation:</b> create a sense of purpose and community and
+        ensures valuable tradition do not disappears
+        <br />
+        <b>eco-friendly:</b> Every purpose of the handlooms is sustainable from
+        installation fabric choice weaving ans dyeing
+      </p>
     </div>
   );
 };
